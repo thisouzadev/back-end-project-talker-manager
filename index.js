20eb303bcc181b896c4f59224ac979ed420c3042
Thiago Souza <78744052+thisouzadev@users.noreply.github.com>
2021-12-23 08:43:52 -0300

    atividade 1

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ const bodyParser = require('body-parser');
 
 const app = express();
 app.use(bodyParser.json());
+const fs = require('fs');
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
-
+const TALKER_FILE = 'talker.json';
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
@@ -15,3 +16,11 @@ app.get('/', (_request, response) => {
 app.listen(PORT, () => {
   console.log('Online');
 });
+
+// https://www.geeksforgeeks.org/node-js-fs-readfilesync-method/
+const getTalker = (_req, res) => {
+  const data = JSON.parse(fs.readFileSync(TALKER_FILE, 'utf-8'));
+  res.status(HTTP_OK_STATUS).json(data);
+};
+
+app.get('/talker', getTalker);
